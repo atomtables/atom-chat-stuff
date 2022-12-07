33e263f494dfcb9328bcf3e039f9620d0111e3ac
adithv08 <adithv08@gmail.com>
2022-12-06 22:03:39 -0500

    more stuff

--- main.js
+++ main.js
@@ -3,7 +3,11 @@ const { Server } = require("socket.io");
 const io = new Server();
 
 io.on("connection", (socket) => {
-  socket.emit("new_message", "i see u shawty");
+  console.log("something");
+  socket.on('hi', (arg) => {
+    socket.emit("new_message", "deez nuts")
+  })
 });
 
 io.listen(3000);
+console.log("what")
